54bd8e7a7a00e2b78c3267aaec955197d6557cf1
cocoa520 <1499934954@qq.com>
2017-03-19 23:52:31 +0800

    add inappstorewin demo

--- AppStoreWinDemo/AppDelegate.h
+++ AppStoreWinDemo/AppDelegate.h
@@ -9,7 +9,9 @@
 #import <Cocoa/Cocoa.h>
 
 @interface AppDelegate : NSObject <NSApplicationDelegate>
-
+{
+    IBOutlet NSWindow* window;
+}
 
 @end
 
--- AppStoreWinDemo/AppDelegate.m
+++ AppStoreWinDemo/AppDelegate.m
@@ -1,26 +1,34 @@
 //
 //  AppDelegate.m
-//  AppStoreWinDemo
+//  Xcode8Demo
 //
-//  Created by admin on 2017/3/19.
+//  Created by admin on 2017/1/15.
 //  Copyright © 2017年 wondershare. All rights reserved.
 //
 
 #import "AppDelegate.h"
-
-@interface AppDelegate ()
-
-@property (weak) IBOutlet NSWindow *window;
-@end
+#import "INAppStoreWindow.h"
 
 @implementation AppDelegate
 
 - (void)applicationDidFinishLaunching:(NSNotification *)aNotification {
     // Insert code here to initialize your application
+    INAppStoreWindow* aWindow = (INAppStoreWindow*)window;
+    aWindow.titleBarHeight = 60.0;
+    aWindow.trafficLightButtonsLeftMargin = 13.0;
+    aWindow.title = @"Hello Window.";
+    //aWindow.titleTextColor = [NSColor blueColor];
+//    aWindow.titleTextShadow = nil;
+    aWindow.titleBarDrawingBlock = ^(BOOL drawsAsMainWindow, CGRect drawingRect,CGRectEdge edge, CGPathRef clippingPath){
+//        [[NSColor blueColor] setFill];
+//        NSRectFill(NSRectFromCGRect(drawingRect));
+    };
 }
+
 
 - (void)applicationWillTerminate:(NSNotification *)aNotification {
     // Insert code here to tear down your application
 }
+
 
 @end
